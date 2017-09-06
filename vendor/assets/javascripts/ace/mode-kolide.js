10213945b0670b9a0a61326c966116e867b75e74
blaedj <blaedj@users.noreply.github.com>
2017-09-06 15:29:48 -0500

    update list of valid tables (#1)
    update the syntax highlighting for valid osquery tables

--- vendor/assets/javascripts/ace/mode-kolide.js
+++ vendor/assets/javascripts/ace/mode-kolide.js
@@ -27,23 +27,32 @@ define("ace/mode/kolide_highlight_rules",["require","exports","module","ace/lib/
         );
 
     var osqueryTables = (
-      "acpi_tables|ad_config|alf|alf_exceptions|alf_explicit_auths|alf_services|app_schemes|apps|apt_sources|" +
-      "arp_cache|asl|authorization_mechanisms|authorizations|authorized_keys|block_devices|browser_plugins|" +
-      "carbon_black_info|certificates|chrome_extensions|cpu_time|cpuid|crashes|crontab|deb_packages|device_file|" +
-      "device_hash|device_partitions|disk_encryption|disk_events|dns_resolvers|etc_hosts|etc_protocols|etc_services|" +
-      "extended_attributes|fan_speed_sensors|file|file_events|firefox_addons|groups|hardware_events|hash|" +
-      "homebrew_packages|interface_addresses|interface_details|iokit_devicetree|iokit_registry|iptables|kernel_extensions|" +
-      "kernel_info|kernel_integrity|kernel_modules|kernel_panics|keychain_acls|keychain_items|known_hosts|last|launchd|" +
-      "launchd_overrides|listening_ports|logged_in_users|magic|managed_policies|memory_info|memory_map|mounts|msr|" +
-      "nfs_shares|nvram|opera_extensions|os_version|osquery_events|osquery_extensions|osquery_flags|osquery_info|" +
-      "osquery_packs|osquery_registry|osquery_schedule|package_bom|package_install_history|package_receipts|pci_devices|" +
-      "platform_info|power_sensors|preferences|process_envs|process_events|process_file_events|process_memory_map|" +
-      "process_open_files|process_open_sockets|processes|programs|quicklook_cache|registry|routes|rpm_package_files|" +
-      "rpm_packages|safari_extensions|sandboxes|shared_memory|shared_resources|shell_history|signature|sip_config|" +
-      "smbios_tables|smc_keys|socket_events|startup_items|suid_bin|syslog|system_controls|system_info|temperature_sensors|" +
-      "time|uptime|usb_devices|user_events|user_groups|user_ssh_keys|users|wifi_networks|wmi_cli_event_consumers|" +
-      "wmi_event_filters|wmi_filter_consumer_binding|wmi_script_event_consumers|xprotect_entries|xprotect_meta|" +
-      "xprotect_reports|yara|yara_events"
+
+      "acpi_tables|ad_config|alf|alf_exceptions|alf_explicit_auths|alf_services|app_schemes|apps|" +
+      "arp_cache|asl|augeas|authorization_mechanisms|authorizations|authorized_keys|block_devices|" +
+      "browser_plugins|carbon_black_info|carves|certificates|chrome_extensions|cpu_time|cpuid|" +
+      "crashes|crontab|device_file|device_firmware|device_hash|device_partitions|disk_encryption|" +
+      "disk_events|dns_resolvers|docker_container_labels|docker_container_mounts|" +
+      "docker_container_networks|docker_container_ports|docker_container_processes|" +
+      "docker_container_stats|docker_containers|docker_image_labels|docker_images|docker_info|"+
+      "docker_network_labels|docker_networks|docker_version|docker_volume_labels|docker_volumes|" +
+      "etc_hosts|etc_protocols|etc_services|event_taps|extended_attributes|fan_speed_sensors|" +
+      "file|file_events|firefox_addons|gatekeeper|gatekeeper_approved_apps|groups|" +
+      "hardware_events|hash|homebrew_packages|interface_addresses|interface_details|" +
+      "iokit_devicetree|iokit_registry|kernel_extensions|kernel_info|kernel_panics|" +
+      "keychain_acls|keychain_items|known_hosts|last|launchd|launchd_overrides|" +
+      "listening_ports|lldp_neighbors|load_average|logged_in_users|magic|managed_policies|" +
+      "mounts|nfs_shares|nvram|opera_extensions|os_version|osquery_events|osquery_extensions|" +
+      "osquery_flags|osquery_info|osquery_packs|osquery_registry|osquery_schedule|package_bom|" +
+      "package_install_history|package_receipts|pci_devices|platform_info|plist|power_sensors|" +
+      "preferences|process_envs|process_events|process_file_events|process_memory_map|" +
+      "process_open_files|process_open_sockets|processes|prometheus_metrics|python_packages|" +
+      "quicklook_cache|routes|safari_extensions|sandboxes|shared_folders|sharing_preferences|" +
+      "shell_history|signature|sip_config|smbios_tables|smc_keys|startup_items|sudoers|" +
+      "suid_bin|system_controls|system_info|temperature_sensors|time|time_machine_backups|" +
+      "time_machine_destinations|uptime|usb_devices|user_events|user_groups|user_ssh_keys|" +
+      "users|virtual_memory_info|wifi_networks|wifi_status|wifi_survey|xprotect_entries|" +
+      "xprotect_meta|xprotect_reports|yara|yara_events"
       );
 
     var keywordMapper = this.createKeywordMapper({
